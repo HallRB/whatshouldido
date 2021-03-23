6833d6a023ccf96eebeadb37b99033dc2b392f06
Richard <richardbrianhall@gmail.com>
2021-03-22 23:53:07 -0500

    textareas in place, button placeholders in place, missing formatting and functions

--- whatshouldido/src/components/main.js
+++ whatshouldido/src/components/main.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import "./main.css";
 class Main extends Component {
+
     render() {
     return(
     <div className="mainWrap">
         <h1>Enter in your choices</h1>
-        <textarea></textarea>
-        <textarea></textarea>
-        <textarea></textarea>
-        <textarea></textarea>
-        <textarea></textarea>
-        <textarea></textarea>
-        <textarea></textarea>
-        <textarea></textarea>
+        <div id="textareas">
+        <textarea id="txt1"></textarea>
+        <textarea id="txt2"></textarea>
+        <textarea id="txt3"></textarea>
+        <textarea id="txt4"></textarea>
+        <textarea id="txt5"></textarea>
+        <textarea id="txt6"></textarea>
+        <textarea id="txt7"></textarea>
+        <textarea id="txt8"></textarea>
+        </div>
+        <button onClick="TellMe()">TELL ME WHAT TO DO</button>
+        <button>ADD CHOICES</button>
     </div>    
-    
     )
+
 }
 }
 
